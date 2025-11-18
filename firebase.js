414fa3b283225c113b70e871369c7b0505b2eeca
esteban <esteban_alfaro@mckinsey.com>
2025-11-18 15:49:45 -0600

    yes

---
+++ firebase.js
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBjLGyOQXd0FE6vjqn_koVsCcWW76Bwz3A",
+    authDomain: "mu25-41abe.firebaseapp.com",
+    projectId: "mu25-41abe",
+    storageBucket: "mu25-41abe.firebasestorage.app",
+    messagingSenderId: "741861425274",
+    appId: "1:741861425274:web:6798c0ebe505ff053bfbc3",
+    measurementId: "G-31T9BGP25N"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
